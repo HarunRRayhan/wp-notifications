84faf517f236163c452da5d302d20f805800e2fa
Harun R Rayhan <harun.b13@gmail.com>
2020-08-20 14:28:42 +0600

    Scaffolding

---
+++ ray-notifications.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Ray Notifications
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ray-notifications
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ray_Notifications
+ */
+
+// Your code starts here.
